d084e7246ecf82b28da8b2d28051a7f494a0db3a
lanou3g <837637793@qq.com>
2016-06-22 17:48:10 +0800

    no message

---
+++ UIProject/UIProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIProject
+//
+//  Created by lanou3g on 16/6/22.
+//  Copyright © 2016年 王晓南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
