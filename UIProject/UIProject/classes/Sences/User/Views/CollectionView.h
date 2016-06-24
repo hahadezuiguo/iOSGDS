54dc7be432347f3fde20f6b5ef5ee31b06a3e2ff
lanou3g <837637793@qq.com>
2016-06-24 09:20:36 +0800

    s

--- UIProject/UIProject/classes/Sences/User/Views/CollectionView.h
+++ UIProject/UIProject/classes/Sences/User/Views/CollectionView.h
@@ -10,4 +10,10 @@
 
 @interface CollectionView : UIView
 
+///声明集合视图属性
+@property (nonatomic,strong) UICollectionView *collection;
+
+//UICollectionViewFlowLayout用来给collectionView布局
+@property (nonatomic,strong)UICollectionViewFlowLayout *flowLayout;
+
 @end
