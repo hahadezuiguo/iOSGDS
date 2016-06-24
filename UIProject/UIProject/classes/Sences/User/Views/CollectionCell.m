57207bce7d02d25c315b0b5f1eb90d5180c2e35c
lanou3g <837637793@qq.com>
2016-06-24 11:59:24 +0800

    add xib

---
+++ UIProject/UIProject/classes/Sences/User/Views/CollectionCell.m
@@ -0,0 +1,17 @@
+//
+//  CollectionCell.m
+//  UIProject
+//
+//  Created by lanou3g on 16/6/23.
+//  Copyright © 2016年 王晓南. All rights reserved.
+//
+
+#import "CollectionCell.h"
+
+@implementation CollectionCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
