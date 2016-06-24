e03f4e49cf7e42047e16e6afffef7b680563e8c2
lanou3g <837637793@qq.com>
2016-06-24 09:32:36 +0800

    no message

--- UIProject/UIProject/classes/Marco/DB_URL.h
+++ UIProject/UIProject/classes/Marco/DB_URL.h
@@ -15,8 +15,6 @@
 //POST
 #define DB_DETAIL @"?content=%7B%22appkey%22%3A%225424c4fbfd98c58f0901fc47%22%2C%22channel%22%3A%22App%20Store%22%2C%22ad_request%22%3A1%2C%22time%22%3A%2219%3A03%3A41%22%2C%22package%22%3A%22com.lighting.BabyFoodAll%22%2C%22type%22%3A%22online_config%22%2C%22sdk_type%22%3A%22iOS%22%2C%22sdk_version%22%3A%223.1.6%22%7D"
 
-//登录接口
-#define DB_LOGIN_URL @"http://162.211.125.85/douban/user.php/DLogin"
 
 
 #endif /* DB_URL_h */
