d72980bb474c8e317b726781496c506014d60b99
lanou3g <837637793@qq.com>
2016-06-28 16:34:40 +0800

    dana

---
+++ UIProject/UIProject/classes/Sences/User/Controllers/UserDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserDetailViewController.h
+//  UIProject
+//
+//  Created by lanou3g on 16/6/28.
+//  Copyright © 2016年 王晓南. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserDetailViewController : UIViewController
+
+@end
