d7a708ab1aeea72183d0dc90db8e8f3a714be032
lanou3g <572182402@qq.com>
2016-06-28 11:13:40 +0800

    zuizhongban

--- UIProject/UIProject/classes/Sences/Secret/Controller/HealthViewController.m
+++ UIProject/UIProject/classes/Sences/Secret/Controller/HealthViewController.m
@@ -19,11 +19,23 @@
 
 #import "HealthDetailViewController.h"
 
-@interface HealthViewController ()<UITableViewDelegate,UITableViewDataSource>
+#import "EGOViewCommon.h"
+#import "EGORefreshTableHeaderView.h"
+#import "EGORefreshTableFooterView.h"
+const CGFloat kTableHeaderViewHeight = 65.f;
+const CGFloat kTableFooterViewHeight = 65.f;
+
+@interface HealthViewController ()<UITableViewDelegate,UITableViewDataSource,EGORefreshTableDelegate>
 
 @property (nonatomic, strong) UITableView *healthTable;
 // 声明可变数组存放解析出的育儿课堂数据
 @property (nonatomic, strong) NSMutableArray *allDataArray;
+//下载状态
+@property (nonatomic, assign) BOOL isLoading;
+//下拉刷新添加的顶部视图
+@property (nonatomic, strong) EGORefreshTableHeaderView *refreshHeaderView;
+//上拉刷新的底部视图
+@property (nonatomic, strong) EGORefreshTableFooterView *refreshFooterView;
 
 @end
 
@@ -42,9 +54,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+
+    _isLoading = NO;
     self.view.backgroundColor = [UIColor whiteColor];
-    self.healthTable = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStylePlain];
+    self.healthTable = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 64) style:UITableViewStylePlain];
+    self.healthTable.separatorStyle = NO;
     [self.view addSubview:self.healthTable];
     
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];//加到self.view上并且"View must not be nil."
@@ -59,9 +73,10 @@
     
     //健康护理网络请求
     [self netHealthRequest];
-
+    [self setHeaderView];
     
 }
+
 
 //网络请求健康护理
 //健康护理网络请求
@@ -130,22 +145,65 @@
     
 }
 
-
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Private Methods
+- (void)setHeaderView
+{
+    if (_refreshHeaderView == nil)
+    {
+        // create the header view
+        UIColor *viewTextColoer = [UIColor colorWithRed:216.0/255.0 green:196.0/255.0 blue:172.0/255.0 alpha:0.7f];
+        UIColor *viewBackgroundColor = [UIColor colorWithRed:251.0/255.0 green:243.0/255.0 blue:231.0/255.0 alpha:1.0f];
+        _refreshHeaderView = [[EGORefreshTableHeaderView alloc] initWithFrame:CGRectMake(0.0f, -kTableHeaderViewHeight, self.healthTable.frame.size.width, kTableFooterViewHeight)
+                                                               arrowImageName:@"refresh_up_arrow"
+                                                             loadingImageNmae:@"refresh_loading"
+                                                                    textColor:viewTextColoer];
+        _refreshHeaderView.backgroundColor = viewBackgroundColor;
+        _refreshHeaderView.delegate = self;
+        
+        [self.healthTable addSubview:_refreshHeaderView];
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - ScrollView Delegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    [_refreshHeaderView egoRefreshScrollViewDidScroll:scrollView];
 }
-*/
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [_refreshHeaderView egoRefreshScrollViewDidEndDragging:scrollView];
+}
+
+#pragma mark - EGORefreshTableView Delegate
+- (void)egoRefreshTableDidTriggerRefresh:(EGORefreshPos)aRefreshPos
+{
+    [self beginReloadData];
+}
+
+- (BOOL)egoRefreshTableDataSourceIsLoading:(UIView*)view
+{
+    return _isLoading;
+}
+
+- (NSDate*)egoRefreshTableDataSourceLastUpdated:(UIView*)view{
+    return [NSDate date];
+}
+
+- (void)beginReloadData
+{
+    _isLoading = YES;
+    
+    // 异步请求刷新数据
+    __weak typeof(self)weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [weakSelf.healthTable reloadData];
+        _isLoading = NO;
+        [_refreshHeaderView egoRefreshScrollViewDataSourceDidFinishedLoading:weakSelf.healthTable];
+    });
+}
+
+
+
 
 @end
