2bc9a1aad78b3937b7a6852a70ecb7280c9b0f4b
zhouyuqi410 <zhouyuqi410@163.com>
2016-07-06 19:40:31 +0800

    2016年07月06日19:40:27

---
+++ UIProject/UIProject/classes/Sences/Travel/Views/WeatherCell.m
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.m
+//  UIProject
+//
+//  Created by lanou3g on 16/7/6.
+//  Copyright © 2016年 王晓南. All rights reserved.
+//
+
+#import "WeatherCell.h"
+
+@implementation WeatherCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
