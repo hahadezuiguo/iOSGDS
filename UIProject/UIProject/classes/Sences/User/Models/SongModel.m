e1c6baea7ad97582584931f980fe81116805d107
lanou3g <837637793@qq.com>
2016-07-06 10:49:33 +0800

    test

---
+++ UIProject/UIProject/classes/Sences/User/Models/SongModel.m
@@ -0,0 +1,23 @@
+//
+//  SongModel.m
+//  UIProject
+//
+//  Created by lanou3g on 16/7/6.
+//  Copyright © 2016年 王晓南. All rights reserved.
+//
+
+#import "SongModel.h"
+
+@implementation SongModel
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    
+    
+}
+
+- (NSString *)description{
+    
+    return [NSString stringWithFormat:@"%@",self.myDescription];
+}
+
+@end
