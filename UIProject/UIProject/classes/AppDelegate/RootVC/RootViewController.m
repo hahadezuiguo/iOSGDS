a98d2eebd41c6ffe3c0a72864c1857903470960d
zhouyuqi410 <zhouyuqi410@163.com>
2016-07-08 16:51:29 +0800

    2016年07月08日16:51:26

--- UIProject/UIProject/classes/AppDelegate/RootVC/RootViewController.m
+++ UIProject/UIProject/classes/AppDelegate/RootVC/RootViewController.m
@@ -62,13 +62,13 @@
     }];
 }
 - (void)createChildViewController {
-    [self addOneChildViewController:[[SecretViewController alloc] init] title:@"育儿秘籍" normalImage:@"esoterica2.png" selectorImage:@"esoterica.png"];
+    [self addOneChildViewController:[[SecretViewController alloc] init] title:@"育儿秘籍" normalImage:@"esoterica.png" selectorImage:@"esoterica2.png"];
     
-    [self addOneChildViewController:[[SortViewController alloc] init] title:@"family一起看" normalImage:@"" selectorImage:@""];
+    [self addOneChildViewController:[[SortViewController alloc] init] title:@"family一起看" normalImage:@"familyWEi1" selectorImage:@"familyWEi"];
     
     [self addOneChildViewController:[[TravelMenuController alloc] init] title:@"宝贝去哪儿？" normalImage:@"tabbarWere" selectorImage:@"tabbarWhere"];
     
-    [self addOneChildViewController:[[UserViewController alloc] init] title:@"妈咪空间" normalImage:@"" selectorImage:@""];
+    [self addOneChildViewController:[[UserViewController alloc] init] title:@"妈咪空间" normalImage:@"userNan1" selectorImage:@"userNan"];
     
 }
 
