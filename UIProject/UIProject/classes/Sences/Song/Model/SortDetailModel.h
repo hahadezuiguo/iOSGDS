c4a2aac039e64723c962f634f29bd294a8840502
zhouyuqi410 <zhouyuqi410@163.com>
2016-07-09 13:22:52 +0800

    最终版本 解决崩溃的bug  曲国威 NSString * 类型用的assign

--- UIProject/UIProject/classes/Sences/Song/Model/SortDetailModel.h
+++ UIProject/UIProject/classes/Sences/Song/Model/SortDetailModel.h
@@ -12,7 +12,7 @@
 @property (nonatomic,copy)NSString *coverForDetail;// 图片网址
 @property (nonatomic,copy)NSString *title;// 图片名称
 @property (nonatomic,copy)NSString *category;// 图片类别
-@property (nonatomic,assign)NSString *duration;//时长
+@property (nonatomic,copy)NSString *duration;//时长
 @property (nonatomic,copy)NSString *videoInformation; //影片详细信息
 @property (nonatomic,copy)NSString *playUrl; // 播放网址
 // 是否被收藏
